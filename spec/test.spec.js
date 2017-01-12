2d12c17945ce9bcdde8e27d5b5db3ee847fc05be
Craig Iturbe <citurbe@gmail.com>
2017-01-12 13:49:36 -0500

    Done.

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -4,6 +4,7 @@ describe('angularjs homepage todo list', function() {
 
 		element(by.model('todoList.todoText')).sendKeys('Writing tests!!');
 		element(by.css('[value="add"]')).click();
+		
 
 		var todoList = element.all(by.repeater('todo in todoList.todos'));
 		expect(todoList.count()).toEqual(3);
